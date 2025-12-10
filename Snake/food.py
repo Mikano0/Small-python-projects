a31a213283fb08cecea10d7a6635f362670051bf
Mikano <113972333+Mikano0@users.noreply.github.com>
2025-12-10 18:15:58 +0100

    Add files via upload

---
+++ Snake/food.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len = 0.5, stretch_wid = 0.5)
+        self.color("yellow")
+        self.speed("fastest")
+        self.new_food()
+        
+    def new_food(self):
+        random_x = random.randint(-360, 360)
+        random_y = random.randint(-260, 260)
+        self.goto(random_x, random_y)
+        
---
+++ Snake/score.py
@@ -0,0 +1,39 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("Day 20/data.txt") as file:
+            self.highscore = int(file.read())
+        self.goto(0,270)
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+        
+    
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.highscore}" , align = "center", font = ("Arial", 20, "normal"))
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("Day 20/data.txt", mode="w") as file:
+                file.write(str(self.highscore))
+
+        self.score = 0
+        self.update_scoreboard()
+
+
+    def update_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
+
+        
+    
+
+
