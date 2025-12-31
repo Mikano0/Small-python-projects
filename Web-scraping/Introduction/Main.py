b0db642bf8b002d8cce3af85539712cf2dbe1468
Mikano <113972333+Mikano0@users.noreply.github.com>
2025-12-31 12:12:07 +0100

    Create Main.py

---
+++ Web-scraping/Introduction/Main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+response.raise_for_status()
+
+web_page = (response.text)
+
+soup = BeautifulSoup(web_page, "html.parser")
+
+all_movies = soup.find_all("h3", class_ = "title")
+all_movies.reverse()
+
+
+for movie in all_movies:
+    movienumber = movie.getText()
+    with open ("Day 45/movies.txt", "a", encoding="utf-8") as file:
+        file.write(movienumber + "\n") 
